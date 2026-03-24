4ef83cbcf5e113d1288642bb0274af2dcbba4ee6
Hubert Jablonski <s33210@pjwstk.edu.pl>
2026-03-24 13:11:16 +0100

    Dodanie klas odpowiadajacych za uzytkownika systemu

---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace DefaultNamespace;
+
+public class Employee : User
+{
+    public override string Role => "Employee";
+    public override int MaxRentals => 5;
+
+    public Employee(string firstName, string lastName) : base(firstName, lastName)
+    {
+        
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace DefaultNamespace;
+
+public class Student : User
+{
+    public override string Role => "Student";
+    public override int MaxRentals => 2;
+
+    public Student(string firstName, string lastName) : base(firstName, lastName)
+    {
+        
+    }
+}
