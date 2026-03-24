5ec663fb536e7334011da783fbd13d437d9ccec4
Hubert Jablonski <s33210@pjwstk.edu.pl>
2026-03-24 20:51:03 +0100

    Dodanie klasy Rental

---
+++ Models/Rental.cs
@@ -0,0 +1,29 @@
+﻿namespace DefaultNamespace;
+
+public class Rental
+{
+    public Guid Id { get; private set; }
+    public User RentedBy { get; private set; }
+    public Equipment RentedEquipment { get; private set; }
+    public DateTime RentDate { get; private set; }
+    public DateTime DueDate { get; private set; }
+    public DateTime? ReturnDate { get; private set; }
+    public int PenaltyFee { get; private set; }
+
+    public Rental(User rentedby, Equipment rentedEquipment, int daysToRent)
+    {
+        Id = Guid.NewGuid();
+        RentedBy = rentedby;
+        RentedEquipment = rentedEquipment;
+        RentDate = DateTime.Now;
+        DueDate = RentDate.AddDays(daysToRent); 
+        ReturnDate = null; 
+        PenaltyFee = 0;
+    }
+
+    public void MarkAsReturned(DateTime returnDate, int penaltyFee)
+    {
+        ReturnDate = returnDate;
+        PenaltyFee = penaltyFee;
+    }
+}
